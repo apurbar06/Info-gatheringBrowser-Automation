9e0dbd0a82a95f286e2c5c196df7772420910d61
apurbar06 <apurbar06@gmail.com>
2023-01-01 14:36:37 +0530

    add functionality to extract the value of price/book

--- bot.py
+++ bot.py
@@ -15,6 +15,8 @@ wait = WebDriverWait(driver, 5)
 driver.get('https://www.morningstar.in/stocks/0p0000bp7x/bse-itc-ltd/overview.aspx')
 time.sleep(10)
 
+
+# Search and Enter
 search_term ='Hindustan Unilever Ltd'
 search_number = '500696'
 sbox = driver.find_element(by=By.XPATH, value='//input[@placeholder="Quote"]')
@@ -24,46 +26,27 @@ driver.find_element(by=By.XPATH, value="//div[contains(text(),'"+search_number+"
 time.sleep(10)
 
 
+# Extract rating
+rating = driver.find_element(by=By.XPATH, value="//div[contains(@class, 'mds-star-rating__sal mds-star-rating--large__sal')]").get_attribute("aria-label")
+
+
+# Extract the value of price/book
+price_pre_book = driver.find_element(by=By.XPATH, value="(.//ul[@class='small-block-grid-3 large-block-grid-4 sal-component-band-grid']/li)[10]//div[@class='dp-value']").text
+
+    
+
+
+print()
+print()
+print()
+print()
+print('rating: ' + rating)
+print('price_pre_book: '+ price_pre_book)
+print()
+print()
+print()
+print()
 
 
 
 
-
-
-
-
-# Opening page and logging in
-# driver.get('https://www.morningstar.in/stocks/0p0000bp7x/bse-itc-ltd/overview.aspx')
-# driver.find_element_by_id("username").send_keys('Your UserID')
-# driver.find_element_by_id("password").send_keys('Your Password')
-# driver.find_element_by_xpath('//button[text()="Sign in"]').click()
-
-# # Search and Enter
-# time.sleep(50)
-# SearchTextBox = wait.until(EC.visibility_of_element_located((By.XPATH, '//input[@placeholder="Quote"]')))
-# action.move_to_element(SearchTextBox).click().send_keys('HINDUSTAN UNILEVER LTD').perform()
-# SearchTextBox.send_keys(Keys.ENTER)
-
-# # Click on Jobs
-# wait.until(EC.element_to_be_clickable((By.XPATH, '//button[text()="Jobs"]'))).click()
-
-# # Clicking company filter
-# company_button = WebDriverWait(driver, 10).until(EC.presence_of_element_located(
-#     (By.XPATH, '//button[@aria-label="Company filter. Clicking this button displays all Company filter options."]')))
-# company_button.click()
-
-# # Send Accenture keyword in the inputbox
-# # Please modify the xpath as per your understanding. Relative path
-# wait.until(EC.presence_of_element_located((By.XPATH, '//input[@placeholder="Add a company"]'))).send_keys('Accenture')
-# time.sleep(5)
-
-# # Collect all the options
-# All_selection = driver.find_elements_by_xpath(
-#     "//span[contains(@class,'search-typeahead-v2__hit-text t-14 t-black')]")
-
-# # Printing all the options
-# for i in All_selection:
-#     print(i.text)
-
-# # Click on the first Option.
-# All_selection[0].click()
