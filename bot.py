9ea8db9ee631e1768e2fd9624b6825dbd1629c67
apurbar06 <apurbar06@gmail.com>
2023-01-09 12:29:26 +0530

    hide output Chrome messages and resolve bug related to last item in the list

--- bot.py
+++ bot.py
@@ -3,15 +3,12 @@ from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver import ActionChains
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
 import time
 import pandas as pd
 import openpyxl
 from openpyxl.styles import Font, Alignment
 from tqdm import tqdm
-from time import sleep
 
 
 
@@ -35,21 +32,28 @@ df = pd.ExcelFile(_file_name).parse(_sheet_name)
 name=df['Stock Name'].tolist()
 number=df['BSE / NSE Code'].tolist()
 
-driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_experimental_option('excludeSwitches', ['enable-logging'])
+driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)
 driver.maximize_window()
 action = ActionChains(driver)
 wait = WebDriverWait(driver, 5)
 driver.get('https://www.morningstar.in')
 time.sleep(10)
+print()
 
 
-# for i in range(_row-2, len(name)):
+
 for i in tqdm(range(0, len(name)), initial = _row-2, desc ="Progress: "):
-    print("\n\n\n\n"+str(name[i])+" -> "+str(number[i]))
+    if(_row-2>=len(name)): 
+        break
+    # print("\n\n\n\n"+str(name[_row-2])+" -> "+str(number[_row-2]))
+    
+    
 
     # Search and Enter
-    # search_term = str(name[i])
-    search_number = str(number[i])
+    # search_term = str(name[_row-2])
+    search_number = str(number[_row-2])
     sbox = driver.find_element(by=By.XPATH, value='//input[@placeholder="Quote"]')
     sbox.send_keys(search_number)
     time.sleep(5)
@@ -61,24 +65,18 @@ for i in tqdm(range(0, len(name)), initial = _row-2, desc ="Progress: "):
     # Extract rating
     rating = driver.find_element(by=By.XPATH, value="//div[contains(@class, 'mds-star-rating__sal mds-star-rating--large__sal')]").get_attribute("aria-label")
     rating = rating.split(" ")[2]
-
     # Extract the value of price/book
     price_pre_book = driver.find_element(by=By.XPATH, value="(.//ul[@class='small-block-grid-3 large-block-grid-4 sal-component-band-grid']/li)[10]//div[@class='dp-value']").text
-
     # Extract the value of price/cash flow
     price_pre_cash_flow = driver.find_element(by=By.XPATH, value="(.//ul[@class='sal-xsmall-block-grid-2 small-block-grid-4']/li)[2]//div[@class='dp-value']").text
-
     # Extract the value of price/sales
     price_pre_sales = driver.find_element(by=By.XPATH, value="(.//ul[@class='sal-xsmall-block-grid-2 small-block-grid-4']/li)[3]//div[@class='dp-value']").text
-
     # Extract the value of price/earnings
     price_pre_earnings = driver.find_element(by=By.XPATH, value="(.//ul[@class='sal-xsmall-block-grid-2 small-block-grid-4']/li)[4]//div[@class='dp-value']").text
-
     # Extract the value of EBITDA
     driver.find_element(by=By.XPATH, value="(.//a[@id='ctl00_ContentPlaceHolder1_ucNavigation_rptNavigation_ctl02_lnkTab'])").click()
     time.sleep(10)
     ebitda = driver.find_element(by=By.XPATH, value="(.//table[@class='sal-summary-section__table']/tbody/tr[4]/td[4])").text
-
     # Extract the value of fair value
     driver.find_element(by=By.XPATH, value="(.//a[@id='ctl00_ContentPlaceHolder1_ucNavigation_rptNavigation_ctl03_lnkTab'])").click()
     time.sleep(10)
@@ -86,7 +84,6 @@ for i in tqdm(range(0, len(name)), initial = _row-2, desc ="Progress: "):
         fair_value = driver.find_element(by=By.XPATH, value="(.//div[@class='sal-columns sal-small-8 sal-medium-12 legend-items']/div)[1]//div[@class='legend-price']/span").text
     else:
         fair_value = driver.find_element(by=By.XPATH, value="(.//div[@class='sal-columns sal-small-8 sal-medium-12 legend-items']/div)[2]//div[@class='legend-price']/span").text
-
     # Extract the value of total yield of TTM
     total_yield_of_ttm = driver.find_element(by=By.XPATH, value="(.//table[@class='mds-table__sal mds-table--fixed-column__sal']/tbody/tr[5]/td[12])").text
 
@@ -137,14 +134,14 @@ for i in tqdm(range(0, len(name)), initial = _row-2, desc ="Progress: "):
         file.writelines(lines)
 
 
-    print('rating-> ' + rating)
-    print('price_pre_book-> '+ price_pre_book)
-    print('price_pre_cash_flow-> '+ price_pre_cash_flow)
-    print('price_pre_sales-> '+ price_pre_sales)
-    print('price_pre_earnings-> '+ price_pre_earnings)
-    print('ebitda-> '+ ebitda)
-    print('fair_value-> '+ fair_value)
-    print('total_yield_of_ttm-> '+ total_yield_of_ttm)
-    print('\n\n\n\n')
+    # print('rating-> ' + rating)
+    # print('price_pre_book-> '+ price_pre_book)
+    # print('price_pre_cash_flow-> '+ price_pre_cash_flow)
+    # print('price_pre_sales-> '+ price_pre_sales)
+    # print('price_pre_earnings-> '+ price_pre_earnings)
+    # print('ebitda-> '+ ebitda)
+    # print('fair_value-> '+ fair_value)
+    # print('total_yield_of_ttm-> '+ total_yield_of_ttm)
+    # print('\n\n\n\n')
 
 
